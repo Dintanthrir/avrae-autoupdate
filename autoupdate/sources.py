602b766075d9adf52d073f49f4d80609144d963e
Dintanthrir <115845548+Dintanthrir@users.noreply.github.com>
2022-11-22 11:19:02 -0800

    Generate reports for alias/snippet/gvar differences

--- autoupdate/sources.py
+++ autoupdate/sources.py
@@ -1,36 +1,334 @@
+"""
+Compares local repository source files to avrae API responses objects.
+Identifies differences and constructs a set of recommended actions to bring avrae in sync
+with the local repository.
+"""
+
 import os
-import pathlib
 
-def contains_ending(files, endings):
-    return any([file.endswith(endings) for file in files])
+from .avrae import (
+    Alias,
+    Collection,
+    Gvar,
+    Snippet,
+)
 
-def scan_directories(scan_path: str, to_scan=None):
-    cwd = scan_path or os.getcwd()
-    active_dirs = []
-    for dirpath, dirnames, filenames in os.walk(cwd):
-        if not contains_ending(filenames, ('alias', 'snippet', 'gvar')):
-            continue
-        shared = os.path.commonprefix([dirpath, cwd])
-        dirname = os.path.relpath(dirpath, shared).replace('\\', '/')
-        if to_scan:
-            if dirname in to_scan:
-                active_dirs.append(dirname)
+
+class ComparisonResult:
+    """
+    The result of a comparison between a single resource in the current repository and the avrae API
+    Each result can explain any difference in state between the two locations
+    Applying a result attempt to bring the two locations into sync,
+      e.g. by updating an alias' contents
+    """
+
+    def summary(self) -> str:
+        """
+        Returns a description of the difference between the local repository and avrae API.
+        """
+
+    def apply(self):
+        """
+        Attempts to syncronize this resource in the local repository with avrae.
+        """
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+                and self.__dict__ == other.__dict__)
+
+# Aliases
+
+
+class _AliasComparisonResult(ComparisonResult):
+    def __init__(self, alias_path: os.PathLike) -> None:
+        super().__init__()
+        self.alias_path = alias_path
+
+
+class LocalAliasNotFoundInAvrae(_AliasComparisonResult):
+    """
+    A .alias file is present in the collection directory
+    but wad not found in the matching avrae collection.
+    """
+
+
+class _AliasComparisonResultWithAlias(_AliasComparisonResult):
+    def __init__(self, alias_path: os.PathLike, alias: Alias) -> None:
+        super().__init__(alias_path)
+        self.alias = alias
+
+
+class LocalAliasMatchesAvrae(_AliasComparisonResultWithAlias):
+    """
+    The local .alias file matches the current active version code in the avrae collection.
+    """
+
+
+class LocalAliasMissing(_AliasComparisonResultWithAlias):
+    """
+    An alias was found in the avrae collection which has no corresponding .alias file
+    in the repository at its expected location.
+    """
+
+
+class LocalAliasDoesNotMatchAvrae(_AliasComparisonResultWithAlias):
+    """
+    The local .alias file contains code which does not match the active version on avrae.
+    """
+
+# Snippets
+
+
+class _SnippetComparisonResult(ComparisonResult):
+    def __init__(self, snippet_path: os.PathLike) -> None:
+        super().__init__()
+        self.snippet_path = snippet_path
+
+
+class LocalSnippetNotFoundInAvrae(_SnippetComparisonResult):
+    """
+    A .snippet file is present in the collection directory
+    but wad not found in the matching avrae collection.
+    """
+
+
+class _SnippetComparisonResultWithSnippet(_SnippetComparisonResult):
+    def __init__(self, snippet_path: os.PathLike, snippet: Snippet) -> None:
+        super().__init__(snippet_path)
+        self.snippet = snippet
+
+
+class LocalSnippetMatchesAvrae(_SnippetComparisonResultWithSnippet):
+    """
+    The local .snippet file matches the current active version code in the avrae collection.
+    """
+
+
+class LocalSnippetMissing(_SnippetComparisonResultWithSnippet):
+    """
+    A snippet was found in the avrae collection which has no corresponding .snippet file
+    in the repository at its expected location.
+    """
+
+
+class LocalSnippetDoesNotMatchAvrae(_SnippetComparisonResultWithSnippet):
+    """
+    The local .snippet file contains code which does not match the active version on avrae.
+    """
+
+# GVars
+
+
+class _GvarComparisonResult(ComparisonResult):
+    def __init__(self, gvar_path: os.PathLike) -> None:
+        super().__init__()
+        self.gvar_path = gvar_path
+
+
+class LocalGvarNotFoundInAvrae(_GvarComparisonResult):
+    """
+    A .gvar file is present in the configuration
+    but wad not found in the matching avrae collection.
+    """
+
+
+class _GvarComparisonResultWithGvar(_GvarComparisonResult):
+    def __init__(self, gvar_path: os.PathLike, gvar: Gvar) -> None:
+        super().__init__(gvar_path)
+        self.gvar = gvar
+
+
+class LocalGvarMatchesAvrae(_GvarComparisonResultWithGvar):
+    """
+    The local .gvar file contents match the gvar in the avrae collection.
+    """
+
+
+class LocalGvarMissing(_GvarComparisonResultWithGvar):
+    """
+    A .gvar file is present in the configuration and avrae
+    but was not found on disk at the expected location.
+    """
+
+
+class LocalGvarDoesNotMatchAvrae(_GvarComparisonResultWithGvar):
+    """
+    The local .gvar file contents do not match the gvar in the avrae collection.
+    """
+
+
+def _compare_aliases(
+    collection: Collection,
+    base_path: os.PathLike
+) -> list[_AliasComparisonResult]:
+    """
+    Generate AliasComparisonResults for the given Collection
+    """
+    def build_alias_map(path_segments: list, alias: Alias):
+        alias_map = {}
+        file_name = alias.name + '.alias'
+        alias_map[os.path.join(*path_segments, alias.name, file_name)] = alias
+        for subalias in alias.subcommands or []:
+            alias_map.update(build_alias_map(
+                path_segments + [alias.name], subalias))
+        return alias_map
+
+    def build_alias_comparison(alias_path: str, alias: Alias) -> _AliasComparisonResult:
+        if not os.path.exists(alias_path):
+            return LocalAliasMissing(alias_path, alias)
         else:
-            active_dirs.append(dirname)
-    return active_dirs
+            with open(alias_path, mode='r', encoding='utf-8') as alias_file:
+                local_code = alias_file.read()
+                if local_code == alias.code:
+                    return LocalAliasMatchesAvrae(alias_path, alias)
+                else:
+                    return LocalAliasDoesNotMatchAvrae(alias_path, alias)
 
-def files_in_actives(scan_path, modified):
-    cwd = scan_path or os.getcwd()
-    files = []
-    for dirpath, dirnames, filenames in os.walk(cwd):
-        for file in filenames:
-            relative = os.path.join(os.path.relpath(dirpath, cwd), file)
-            if '\\' in relative:
-                relative = pathlib.PureWindowsPath(relative)
-            else:
-                relative = pathlib.PurePosixPath(relative)
-            path = relative.as_posix()
-            if path in modified:
-                files.append(path)
-    return files
+    def find_aliases(base_path: os.PathLike) -> list[os.PathLike]:
+        found_aliases = []
+        for dirpath, _, filenames in os.walk(base_path):
+            shared = os.path.commonprefix([dirpath, base_path])
+            dirname = os.path.relpath(dirpath, shared)
+            found_aliases += [os.path.join(dirname, filename)
+                              for filename in filenames if filename.endswith('.alias')]
+        return found_aliases
 
+    path_segments = [base_path, collection.name]
+    aliases_map: dict[str, Alias] = {}
+    for alias in collection.aliases or []:
+        aliases_map.update(build_alias_map(path_segments, alias))
+    comparison_results = [build_alias_comparison(alias_path, alias) for (
+        alias_path, alias) in aliases_map.items()]
+
+    local_aliases = find_aliases(base_path=os.path.join(*path_segments))
+    full_local_aliases = (os.path.join(*path_segments, local_alias)
+                          for local_alias in local_aliases)
+    missing_aliases = (
+        alias for alias in full_local_aliases if not alias in aliases_map)
+    comparison_results += [LocalAliasNotFoundInAvrae(alias)
+                           for alias in missing_aliases]
+    return comparison_results
+
+
+def _compare_snippets(
+    collection: Collection,
+    base_path: os.PathLike
+) -> list[_SnippetComparisonResult]:
+    """
+    Generate SnippetComparisonResults for the given Collection
+    """
+    def build_snippet_comparison(snippet_path: str, snippet: Snippet) -> _AliasComparisonResult:
+        if not os.path.exists(snippet_path):
+            return LocalSnippetMissing(snippet_path, snippet)
+        else:
+            with open(snippet_path, mode='r', encoding='utf-8') as snippet_file:
+                local_code = snippet_file.read()
+                if local_code == snippet.code:
+                    return LocalSnippetMatchesAvrae(snippet_path, snippet)
+                else:
+                    return LocalSnippetDoesNotMatchAvrae(snippet_path, snippet)
+
+    def find_snippets(base_path: os.PathLike) -> list[os.PathLike]:
+        found_snippets = []
+        for dirpath, _, filenames in os.walk(base_path):
+            shared = os.path.commonprefix([dirpath, base_path])
+            dirname = os.path.relpath(dirpath, shared)
+            found_snippets += [os.path.join(dirname, filename)
+                               for filename in filenames if filename.endswith('.snippet')]
+        return found_snippets
+
+    snippets_directory = (base_path / collection.name / 'snippets')
+    snippet_paths = ((os.path.join(snippets_directory, snippet.name +
+                     '.snippet'), snippet) for snippet in collection.snippets)
+    snippets_map = {snippet_path: snippet for (
+        snippet_path, snippet) in snippet_paths}
+    comparision_results = [build_snippet_comparison(snippet_path, snippet) for (
+        snippet_path, snippet) in snippets_map.items()]
+
+    local_snippets = find_snippets(base_path=snippets_directory)
+    full_local_snippets = (os.path.normpath(os.path.join(
+        snippets_directory, local_snippet)) for local_snippet in local_snippets)
+    missing_snippets = (
+        snippet for snippet in full_local_snippets if not snippet in snippets_map)
+    comparision_results += [LocalSnippetNotFoundInAvrae(
+        snippet) for snippet in missing_snippets]
+
+    return comparision_results
+
+
+def _compare_gvars(
+    gvars: list[Gvar],
+    config: dict[str: os.PathLike],
+    base_path: os.PathLike
+) -> list[_GvarComparisonResult]:
+    """
+    Generate GvarComparisonResults for defined Gvars
+
+    Returns results for any gvar defined in the gvars config file,
+    does not report gvars found on avrae but not present in the repo as multiple repos may be used
+    to update a single avrae account so we cannot assume that all visible gvars should be included.
+    """
+    def build_gvar_comparison(gvar_key: str, gvar_path: os.PathLike):
+        gvar: Gvar | None = next(
+            filter(lambda gvar: gvar.key == gvar_key, gvars), None)
+        if not gvar:
+            return LocalGvarNotFoundInAvrae(gvar_path)
+        elif not os.path.exists(gvar_path):
+            return LocalGvarMissing(gvar_path, gvar)
+        else:
+            with open(gvar_path, mode='r', encoding='utf-8') as gvar_file:
+                local_code = gvar_file.read()
+                if local_code == gvar.value:
+                    return LocalGvarMatchesAvrae(gvar_path=gvar_path, gvar=gvar)
+                else:
+                    return LocalGvarDoesNotMatchAvrae(gvar_path=gvar_path, gvar=gvar)
+
+    gvars_map = ((gvar_key, os.path.join(base_path, relative_path))
+                 for (gvar_key, relative_path) in config.items())
+    return [build_gvar_comparison(gvar_key, gvar_path) for (gvar_key, gvar_path) in gvars_map]
+
+
+def compare_repository_collection_with_avrae(collection: Collection, base_path: os.PathLike):
+    """
+    Compare a single Collection with the source files in the repository.
+    """
+    return {
+        'aliases': _compare_aliases(collection=collection, base_path=base_path),
+        'snippets': _compare_snippets(collection=collection, base_path=base_path),
+    }
+
+
+def compare_repository_with_avrae(
+    collections: list[Collection],
+    gvars: list[Gvar],
+    gvar_config: dict[str: os.PathLike],
+    base_path: os.PathLike
+):
+    """
+    Compare all Collections and Gvars with the local repository
+    """
+    collection_results = [compare_repository_collection_with_avrae(
+        collection=collection, base_path=base_path) for collection in collections]
+    return {
+        'collections': collection_results,
+        'gvars': _compare_gvars(gvars=gvars, config=gvar_config, base_path=base_path)
+    }
+
+    # for each alias/snippet
+    # does a local copy exist?
+    # is it modified?
+    # if not modified is the code unchanged?
+    # does it have docs?
+    # are the docs modified or do they match the dashboard copy?
+    # for each collection in collections.json
+    # are there any misplaced snippets not at the top level?
+    # do all snippets appear in the dashboard?
+    # are their any aliases not in matching folders?
+    # are their folders containing aliases which are not in the dashboard?
+    # do they have doc files?
+    # for each gvar in gvars.json
+    # is there a local copy?
+    # is it modified or does it match the dashboard?
+    # for each .gvar file
+    # is it misplaced?
+    # is it in gvars.json?
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,49 +1,127 @@
-from autoupdate.sources import (
-    files_in_actives,
-    scan_directories
-)
+"""
+Tests for sources.py
+"""
 
-def test_scan_directories(tmp_path):
-    FILE_CONTENTS = {
-        'test.alias': 'alias',
-        'test.txt': 'text',
-        'test': 'anything',
-        'test.gvar': 'gvar',
-        'test.snippet': 'snippet',
-        'test.md': 'markdown',
+from autoupdate.avrae import Collection, Gvar
+from autoupdate.sources import (LocalAliasDoesNotMatchAvrae,
+                                LocalAliasMatchesAvrae, LocalAliasMissing,
+                                LocalAliasNotFoundInAvrae,
+                                LocalGvarDoesNotMatchAvrae,
+                                LocalGvarMatchesAvrae, LocalGvarMissing,
+                                LocalGvarNotFoundInAvrae,
+                                LocalSnippetDoesNotMatchAvrae,
+                                LocalSnippetMatchesAvrae, LocalSnippetMissing,
+                                LocalSnippetNotFoundInAvrae,
+                                _compare_aliases, _compare_gvars, _compare_snippets)
+
+
+def test_compare_aliases(collection_fixtures: dict[str, Collection], tmp_path):
+    collection_id = '5fa19a9814a62cb7e811c5c4'
+    collection = collection_fixtures[collection_id]
+    (tmp_path / 'API Collection Test').mkdir()
+    (tmp_path / 'API Collection Test' / 'test-alias').mkdir()
+    (tmp_path / 'API Collection Test' / 'test-alias' /
+     'test-alias.alias').write_text(collection.aliases[0].code)
+    (tmp_path / 'API Collection Test' / 'test-alias' / 'test-subalias').mkdir()
+    (tmp_path / 'API Collection Test' / 'test-alias' /
+     'test-subalias' / 'test-subalias.alias').write_text('changed')
+    (tmp_path / 'API Collection Test' / 'new-alias').mkdir()
+    (tmp_path / 'API Collection Test' / 'new-alias' /
+     'new-alias.alias').write_text('new addition')
+    (tmp_path / 'Some other collection').mkdir()
+    (tmp_path / 'Some other collection' / 'new-alias').mkdir()
+    (tmp_path / 'Some other collection' / 'new-alias' /
+     'new-alias.alias').write_text('should be ignored')
+    (tmp_path / 'some-alias-file.alias').write_text('should be ignored')
+
+    comparison = _compare_aliases(collection=collection, base_path=tmp_path)
+    assert comparison == [
+        LocalAliasMatchesAvrae(
+            (tmp_path / 'API Collection Test' /
+             'test-alias' / 'test-alias.alias').as_posix(),
+            collection.aliases[0]
+        ),
+        LocalAliasDoesNotMatchAvrae(
+            (tmp_path / 'API Collection Test' / 'test-alias' /
+             'test-subalias' / 'test-subalias.alias').as_posix(),
+            collection.aliases[0].subcommands[0]
+        ),
+        LocalAliasMissing(
+            (tmp_path / 'API Collection Test' / 'test-alias' / 'test-subalias' /
+             'test-subalias' / 'test-subalias.alias').as_posix(),
+            collection.aliases[0].subcommands[0].subcommands[0]
+        ),
+        LocalAliasNotFoundInAvrae(
+            (tmp_path / 'API Collection Test' /
+             'new-alias' / 'new-alias.alias').as_posix()
+        ),
+    ]
+
+
+def test_compare_snippets(collection_fixtures: dict[str, Collection], tmp_path):
+    collection_id = '5fa19a9814a62cb7e811c5c4'
+    collection = collection_fixtures[collection_id]
+    (tmp_path / 'API Collection Test').mkdir()
+    (tmp_path / 'API Collection Test' / 'snippets').mkdir()
+    (tmp_path / 'API Collection Test' / 'snippets' /
+     'test123.snippet').write_text(collection.snippets[0].code)
+
+    assert _compare_snippets(collection=collection, base_path=tmp_path) == [
+        LocalSnippetMatchesAvrae((tmp_path / 'API Collection Test' /
+                                 'snippets' / 'test123.snippet').as_posix(), collection.snippets[0])
+    ]
+
+    (tmp_path / 'API Collection Test' / 'snippets' /
+     'test123.snippet').write_text('modified')
+    assert _compare_snippets(collection=collection, base_path=tmp_path) == [
+        LocalSnippetDoesNotMatchAvrae(
+            (tmp_path / 'API Collection Test' /
+             'snippets' / 'test123.snippet').as_posix(),
+            collection.snippets[0]
+        )
+    ]
+
+    (tmp_path / 'API Collection Test' / 'snippets' /
+     'new.snippet').write_text('new addition')
+    (tmp_path / 'API Collection Test' / 'snippets' / 'test123.snippet').unlink()
+    assert _compare_snippets(collection=collection, base_path=tmp_path) == [
+        LocalSnippetMissing((tmp_path / 'API Collection Test' / 'snippets' /
+                            'test123.snippet').as_posix(), collection.snippets[0]),
+        LocalSnippetNotFoundInAvrae(
+            (tmp_path / 'API Collection Test' / 'snippets' / 'new.snippet').as_posix()),
+    ]
+
+
+def test_compare_gvars(tmp_path):
+    (tmp_path / 'up-to-date.gvar').write_text('gvar content')
+    (tmp_path / 'gvars').mkdir()
+    (tmp_path / 'gvars' / 'modified-var.gvar').write_text('more gvar content')
+    (tmp_path / 'gvars' / 'new-var.gvar').write_text('more gvar content')
+
+    config = {
+        "abc123": "up-to-date.gvar",
+        "def456": "gvars/modified-var.gvar",
+        "cba789": "gvars/new-var.gvar",
+        "fed321": "gvars/not-found.gvar",
     }
 
-    # directories containing all example files
-    for dir_name in ['collection', 'not a collection']:
-        current_path = tmp_path / dir_name
-        current_path.mkdir()
-        for (file_name, contents) in FILE_CONTENTS.items():
-            (current_path / file_name).write_text(contents)
+    gvars = [
+        Gvar(owner='999', key='abc123', owner_name='my name',
+             value='gvar content', editors=[]),
+        Gvar(owner='999', key='def456', owner_name='my name',
+             value='current gvar content', editors=[]),
+        Gvar(owner='999', key='fed321', owner_name='my name',
+             value='current gvar content', editors=[]),
+    ]
 
-    # directories containing each example file in isolation
-    collections = { 'collection': 'collection id'}
-    for (file_name, contents) in FILE_CONTENTS.items():
-        collection_name = f'{file_name} only collection'
-        current_path = tmp_path / collection_name
-        current_path.mkdir()
-        (current_path / file_name).write_text(contents)
-        collections[collection_name] = f'{collection_name} id'
-
-    source_dirs = scan_directories(scan_path=tmp_path, to_scan=collections)
-    assert sorted(source_dirs) == sorted([
-        'collection',
-        'test.alias only collection',
-        'test.gvar only collection',
-        'test.snippet only collection',
-    ])
-
-def test_files_in_actives(tmp_path):
-    (tmp_path / 'file').write_text('file')
-    (tmp_path / 'modified_file').write_text('file')
-    (tmp_path / 'subdir').mkdir()
-    (tmp_path / 'subdir' / 'file').write_text('file')
-    (tmp_path / 'subdir' / 'modified_file').write_text('file')
-    files = files_in_actives(tmp_path, modified=[
-        'modified_file', 'subdir/modified_file', 'deleted_file'
-    ])
-    assert sorted(files) == sorted(['modified_file', 'subdir/modified_file'])
+    comparison = _compare_gvars(gvars=gvars, config=config, base_path=tmp_path)
+    assert comparison == [
+        LocalGvarMatchesAvrae(
+            (tmp_path / 'up-to-date.gvar').as_posix(), gvars[0]),
+        LocalGvarDoesNotMatchAvrae(
+            (tmp_path / 'gvars' / 'modified-var.gvar').as_posix(), gvars[1]),
+        LocalGvarNotFoundInAvrae(
+            (tmp_path / 'gvars' / 'new-var.gvar').as_posix()),
+        LocalGvarMissing(
+            (tmp_path / 'gvars' / 'not-found.gvar').as_posix(), gvars[2]),
+    ]
